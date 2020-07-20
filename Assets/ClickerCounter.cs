7ec228cc1440ead74e4624b530460286810bcbe8
Karasushino <mrcarras26@gmail.com>
2020-07-20 22:58:37 +0100

    1st Commit
    This got deleted so I'm not bothered.
    
    Sprites render, particle render. You get points and you can upgrade to get more points.

---
+++ Assets/ClickerCounter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ClickerCounter : MonoBehaviour
+{
+    public static int numberOfClicks;
+    public static int totalClicks = 0;
+    public static int maxClicks = 12;
+    public static int waifuPoints = 0;
+    public TextMeshProUGUI currentClicks;
+    public TextMeshProUGUI HeadpatScore;
+    public TextMeshProUGUI WaifuScore;
+    public ParticleSystem heartsParticles;
+    private static int barOffset = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfClicks = barOffset;
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 spawnPosition = new Vector3(mousePosition.x, mousePosition.y, 40f);
+            Quaternion rotation = new Quaternion(1f, 1f, 1f, -1f);
+            ParticleSystem ps = Instantiate(heartsParticles, spawnPosition, rotation) as ParticleSystem;
+            ps.Play();
+            Destroy(ps.gameObject, 1.5f);
+           
+
+
+
+
+            numberOfClicks++;
+            totalClicks++;
+            if (numberOfClicks > 12)
+            {
+                numberOfClicks = barOffset;
+                Upgrades.addWaifuPoints();
+            }
+
+        }
+        //Displays number of current clicks left to fill bar to get waifu point
+        currentClicks.text = (numberOfClicks - barOffset) + "/" + (maxClicks - barOffset);
+        //Set Headpat Counter
+        HeadpatScore.text = totalClicks.ToString();
+        //Set Waifu Score
+        WaifuScore.text = waifuPoints.ToString();
+    }
+}
---
+++ Assets/Upgrades.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+
+public class Upgrades : MonoBehaviour
+{
+
+
+
+    //Acts as a multiplyer of waifu 
+    [SerializeField]
+    private static int headPat_level = 1;
+    [SerializeField]
+    private int headPat_cost;
+    //The percentage increased on the cost of the upgrade
+    [SerializeField]
+    private int headPat_cost_multiplier = 20;
+
+    enum Upgrade
+    {
+        HEADPAT,
+        TEST
+
+    }
+
+    public  TextMeshProUGUI UpgradeTitle;
+    public  TextMeshProUGUI UpgradeDescription;
+    public  TextMeshProUGUI Level;
+    public  TextMeshProUGUI Cost;
+
+    Upgrade upgradeType = Upgrade.HEADPAT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (upgradeType)
+        {
+            case Upgrade.HEADPAT:
+                displayHeadpatUpgrade();
+                break;
+            case Upgrade.TEST:
+                displayTESTUpgrade();
+                break;
+        }
+
+
+
+    }
+
+    public void displayHeadpatUpgrade()
+    {
+        UpgradeTitle.text = "Better Headpats";
+        UpgradeDescription.text = "Headpats = waifu points";
+        Level.text = headPat_level.ToString();
+        Cost.text = headPat_cost.ToString();
+    }
+    public void displayTESTUpgrade()
+    {
+        UpgradeTitle.text = "test";
+        UpgradeDescription.text = "test = waifu points";
+        Level.text = headPat_level.ToString();
+        Cost.text = headPat_cost.ToString();
+    }
+
+    public void upgradeHeadpat()
+    {
+        //If enough waifu points upgrade and remove cost from total waifu points
+        if (ClickerCounter.waifuPoints >= headPat_cost)
+        {
+            //Remove waifu points
+            ClickerCounter.waifuPoints -= headPat_cost;
+            //Increase Level
+            headPat_level++;
+            //Now increase cost for the next time
+            headPat_cost += (headPat_cost * headPat_cost_multiplier)/100;
+
+            //Update new values
+            Level.text = headPat_level.ToString();
+            Cost.text = headPat_cost.ToString();
+        }
+    }
+
+    public static void addWaifuPoints()
+    {
+        //Adds waifu Points based on the level
+        ClickerCounter.waifuPoints += 1 * headPat_level;
+    }
+}
